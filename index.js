0b5e910b490e0bb1874dcd65ece9d960347cecbf
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-02-15 01:22:14 +0530

    added Toast and  navbar enhance

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-console.log("hello");
+
+/*------ Toast ------*/
+
+const toasts = document.querySelectorAll(".toasts");
+const successToast = document.querySelectorAll(".toast-btn");
+
+const handleSucessToast = (toastMessage) => {
+  const toastNotification = document.createElement("div");
+  toastNotification.classList.add("toast-success");
+  toastNotification.innerText = `${toastMessage}`;
+  toasts[0].appendChild(toastNotification);
+
+  setTimeout(() => {
+    toastNotification.remove();
+  }, 2000);
+};
+
+successToast[0].addEventListener("click", () =>
+  handleSucessToast("This is success toast.")
+);
 
 const toggleNav = () => {
   const nav = document.querySelector(".component-list");
   nav.classList.toggle("show");
 };
 
+togglerNew.addEventListener("click", () => {
+  console.log("hello");
+  const nav = document.querySelector(".component-list");
+  nav.classList.toggle("show");
+});
 const toggler = document.querySelector(".toggler");
 toggler.addEventListener("click", toggleNav);
 console.log(toggler);
@@ -12,9 +36,3 @@ console.log(toggler);
 const togglerNew = document.querySelector(".toggler-new");
 
 console.log(togglerNew);
-
-togglerNew.addEventListener("click", () => {
-  console.log("hello");
-  const nav = document.querySelector(".component-list");
-  nav.classList.toggle("show");
-});
