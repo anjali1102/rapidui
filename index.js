4ecd110582ced87f74c78fe48a7dee4001256444
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-02-15 17:21:11 +0530

    resolving reviews

--- index.js
+++ index.js
@@ -25,14 +25,11 @@ const toggleNav = () => {
 };
 
 togglerNew.addEventListener("click", () => {
-  console.log("hello");
   const nav = document.querySelector(".component-list");
   nav.classList.toggle("show");
 });
 const toggler = document.querySelector(".toggler");
 toggler.addEventListener("click", toggleNav);
-console.log(toggler);
 
 const togglerNew = document.querySelector(".toggler-new");
 
-console.log(togglerNew);
